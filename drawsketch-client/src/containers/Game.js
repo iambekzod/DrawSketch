b2349f1f0bc5bcc5ccbbb97eee0629939d4f6528
Shuyi Qiu <qiushuyi@IITS-B473-39.utsc-labs.utoronto.ca>
2018-03-05 13:51:03 -0500

    game comp created

---
+++ drawsketch-client/src/containers/Game.js
@@ -0,0 +1,29 @@
+import React, {
+    Component
+} from 'react';
+class TodoStore {
+    todos = [];
+
+    get completedTodosCount() {
+        return this.todos.filter(
+            todo => todo.completed === true
+        ).length;
+    }
+
+    report() {
+        if (this.todos.length === 0)
+            return "<none>";
+        return `Next todo: "${this.todos[0].task}". ` +
+            `Progress: ${this.completedTodosCount}/${this.todos.length}`;
+    }
+
+    addTodo(task) {
+        this.todos.push({
+            task: task,
+            completed: false,
+            assignee: null
+        });
+    }
+}
+
+const todoStore = new TodoStore();

aa3fe20f68eadc54a92d0ba2d15a44cdff5b8624
Bekzod Tursunov <bekzod97@hotmail.com>
2018-03-30 13:27:56 -0400

    fix the app crashing randomly due to a bug with user sanitizing info

--- routes/api/user.js
+++ routes/api/user.js
@@ -14,17 +14,16 @@ var sanitizeUserPass = function(req, res, next) {
 }
 
 var sanitizeInput = function (req, res, next) {
+  if (!validator.isAlphanumeric(req.body.user.username)) return res.status(422).json({errors: {Username: "must contain only letters and numbers"}})
   if (!validator.isAlpha(req.body.user.firstname)) return res.status(422).json({errors: {Firstname: "must contain only letters"}});
   if (!validator.isAlpha(req.body.user.lastname)) return res.status(422).json({errors: {Lastname: "must contain only letters"}});
   if (!validator.isEmail(req.body.user.email)) return res.status(422).json({errors: {Email: "must be of proper email format"}});
+  if (req.body.user.password !== req.body.user.confirmPassword) return res.status(401).json({errors: {Passwords: " do not match"}});
 
-  sanitizeUserPass(req, res, next);
   req.body.user.confirmPassword = validator.escape(req.body.user.confirmPassword);
   req.body.user.firstname = validator.escape(req.body.user.firstname);
   req.body.user.lastname = validator.escape(req.body.user.lastname);
   req.body.user.email = validator.escape(req.body.user.email);
-
-  if (req.body.user.password !== req.body.user.confirmPassword) return res.status(401).json({errors: {Passwords: " do not match"}});
 
   next();
 }
@@ -57,7 +56,7 @@ router.post('/signin', sanitizeUserPass, function(req, res, next) {
   })(req, res, next);
 });
 
-router.post('/signup', sanitizeInput, function(req, res, next) {
+router.post('/signup', sanitizeUserPass, sanitizeInput, async (req, res, next) => {
   var newUser = Accounts({
     username: req.body.user.username,
     firstname: req.body.user.firstname,
@@ -66,13 +65,13 @@ router.post('/signup', sanitizeInput, function(req, res, next) {
     wins: 0,
   });
   newUser.setPassword(req.body.user.password);
-  newUser.save(function (err) {
-      if (err) {
-        if (err.errors["username"]) return res.status(422).json({errors: { Username: "is taken" }});
-        if (err.errors["email"]) return res.status(422).json({errors: { Email: "is being used by another user" }});
+  newUser.save().then((user) => {
+    return res.json(user.toAuthJSON());
+  }).catch((err) => {
+    if (err.errors["username"]) return res.status(422).json({errors: { Username: "is taken" }});
+    if (err.errors["email"]) return res.status(422).json({errors: { Email: "is being used by another user" }});
 
-        return res.status(422).json({errors: { error: err.message }}); // Fall back, to always return something
-      } else return res.json(newUser.toAuthJSON());
+    return res.status(422).json({errors: { error: err.message }}); // Fall back, to always return something
   });
 });
 
